3b79507af83ab587d15b93b073dc2407b412f90a
saadsumbhul <msaadzubair786@gmail.com>
2023-08-14 10:09:16 +0500

    alerts

---
+++ App.js
@@ -0,0 +1,56 @@
+// CHAPTER 1 TOPIC ALERTS
+
+// Alert1
+alert("Error! please enter a valid password");
+// Alert2
+alert("Welcome To js Land... \nHappy Cooding!");
+
+// Alert3
+alert("Welcome To jS Land..")
+alert("Happpy Coding! \n🔲Prevent this page frome creating additional dialogs.");
+// Alert4,
+alert("Hello i Can Run JS through Console")
+
+
+// CHAPTER2
+// topic variable
+
+var userName;
+var myName = "Muhammad Saad"
+var message;
+
+message = "Hello World"
+alert(message)
+
+var studentName = "John Doe"
+alert(studentName)
+
+var studentAge=15
+alert(studentAge)
+
+var studentDesignation = "Certified Mobile Application Development"
+alert(studentDesignation)
+
+var JS = ("pizza \n pizz \n piz \n pi \n p")
+alert(JS)
+
+var email = "Email Address(e.g. example@example.com)"
+alert(email)
+
+var book = "A Smarter Way to Learn JavaScript"
+alert(book)
+
+var specialCharacters = "🐼▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬🐼"
+alert( specialCharacters )
+
+// DOCUMENT
+document.write("Yah! I Can Write HTML Content Through JavaScript")
+
+
+// CHAPTER4
+// TOPIC VARIABLE NAMES: LEGAL & ILLEGAL
+// Step 1
+
+
+  
+  
